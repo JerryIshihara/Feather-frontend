8e1ad5f21c099281550ba1827ce20acf34fbeb47
xuegongao <gongao_xue@berkeley.edu>
2023-04-20 21:42:21 -0700

    Modify settings page

--- src/routes/Dashboard/settings.tsx
+++ src/routes/Dashboard/settings.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useTheme, Container, FormControlLabel, Checkbox, Typography, Divider, RadioGroup, Radio, FormControl, FormLabel } from "@mui/material";
+import { useTheme, TextField, Container, FormControlLabel, Checkbox, Typography, Divider, RadioGroup, Radio, FormControl, FormLabel, Button} from "@mui/material";
+import SaveIcon from '@mui/icons-material/Save';
 
 const Setting = () => {
 const theme = useTheme();
@@ -7,48 +8,80 @@ const theme = useTheme();
 return (
     <div style={{ backgroundColor: theme.palette.background.default, padding: theme.spacing(4) }}>
         <Container maxWidth="lg">
-            <Typography variant="h4" gutterBottom>Account</Typography>
-            <Typography variant="subtitle1">Current Account: UserA</Typography>
-            <FormControl component="fieldset">
-                <FormLabel component="legend">Switch Account</FormLabel>
-                <RadioGroup aria-label="account" name="account" defaultValue="UserA">
-                    <FormControlLabel value="UserA" control={<Radio />} label="UserA" />
-                    <FormControlLabel value="UserB" control={<Radio />} label="UserB" />
-                    <FormControlLabel value="UserC" control={<Radio />} label="UserC" />
-                </RadioGroup>
-            </FormControl>
+
+			{/* -----------------------------------------Password----------------------------------------- */}
+			<Typography variant="h4" gutterBottom>
+				Change Password
+				<div style={{ float: 'right' }}>
+					<Button variant="contained" sx={{ color: theme.palette.text.primary }} startIcon={<SaveIcon />} component="span">
+						Save
+					</Button>
+				</div>
+				<div>
+					<TextField label="Current Password" required type="password" margin="normal" variant="outlined" fullWidth />
+					<TextField label="New Password" type="password" margin="normal" variant="outlined" fullWidth inputProps={{ minLength: 8 }} />
+					<TextField label="Confirm New Password" type="password" margin="normal" variant="outlined" fullWidth inputProps={{ minLength: 8 }} />
+				</div>
+			</Typography>
+
+			{/* -----------------------------------------Contact Info----------------------------------------- */}
+			<Divider style={{ margin: theme.spacing(2, 0) }} />
+			<Typography variant="h4" gutterBottom>
+				Contact Information
+				<div style={{ float: 'right' }}>
+					<Button variant="contained" sx={{ color: theme.palette.text.primary }} startIcon={<SaveIcon />} component="span">
+						Save
+					</Button>
+				</div>
+				<div>
+					<TextField label="Email" required type="email" margin="normal" variant="outlined" fullWidth/>
+					<TextField label="Phone Number" type="tel" margin="normal" variant="outlined" fullWidth/>
+				</div>
+			</Typography>
+
+			{/* -----------------------------------------Notification----------------------------------------- */}
             <Divider style={{ margin: theme.spacing(2, 0) }} />
-            <Typography variant="h4" gutterBottom>Notifications</Typography>
+            <Typography variant="h4" gutterBottom>
+				Notifications
+				<div style={{ float: 'right' }}>
+					<Button variant="contained" sx={{ color: theme.palette.text.primary }} startIcon={<SaveIcon />} component="span">
+						Save
+					</Button>
+				</div>
+			</Typography>
             <FormControl component="fieldset">
-                <FormLabel component="legend">Receive Notifications</FormLabel>
+				<Typography variant="subtitle1">Receive Notifications:</Typography>
                 <RadioGroup aria-label="notifications" name="notifications" defaultValue="email">
-                    <FormControlLabel value="email" control={<Radio />} label="Email" />
-                    <FormControlLabel value="text" control={<Radio />} label="Text" />
+					<FormControlLabel value="email" control={<Checkbox />} label="Email" />
+					<FormControlLabel value="text" control={<Checkbox />} label="Text" />
                 </RadioGroup>
             </FormControl>
+
             <Typography variant="subtitle1">Select messages you want to receive:</Typography>
             <FormControl component="fieldset">
-                <FormControlLabel control={<Checkbox />} label="Product Updates" />
-                <FormControlLabel control={<Checkbox />} label="New Videos" />
-                <FormControlLabel control={<Checkbox />} label="New Comments" />
+                <FormControlLabel control={<Checkbox />} label="Product updates" />
+                <FormControlLabel control={<Checkbox />} label="New videos" />
+                <FormControlLabel control={<Checkbox />} label="New comments" />
+				<FormControlLabel control={<Checkbox />} label="Advertisements" />
+				<FormControlLabel control={<Checkbox />} label="Sport lover events and meetups" />
             </FormControl>
+
+			{/* -----------------------------------------Preference----------------------------------------- */}
             <Divider style={{ margin: theme.spacing(2, 0) }} />
-            <Typography variant="h4" gutterBottom>Billing and Payment</Typography>
-            <Typography variant="subtitle1">Select your payment option:</Typography>
-            <FormControl component="fieldset">
-                <RadioGroup aria-label="payment" name="payment" defaultValue="visa">
-                    <FormControlLabel value="visa" control={<Radio />} label="Visa/Master Card" />
-                    <FormControlLabel value="applepay" control={<Radio />} label="ApplePay" />
-                    <FormControlLabel value="paypal" control={<Radio />} label="Paypal" />
-                </RadioGroup>
-            </FormControl>
-            <Divider style={{ margin: theme.spacing(2, 0) }} />
-            <Typography variant="h4" gutterBottom>Preference</Typography>
+            <Typography variant="h4" gutterBottom>
+				Preference
+				<div style={{ float: 'right' }}>
+					<Button variant="contained" sx={{ color: theme.palette.text.primary }} startIcon={<SaveIcon />} component="span">
+						Save
+					</Button>
+				</div>
+			</Typography>
             <Typography variant="subtitle1">Select topics you're interested in:</Typography>
             <FormControl component="fieldset">
-                <FormControlLabel control={<Checkbox />} label="Topic A" />
-                <FormControlLabel control={<Checkbox />} label="Topic B" />
-                <FormControlLabel control={<Checkbox />} label="Topic C" />
+                <FormControlLabel control={<Checkbox />} label="Skill improving" />
+                <FormControlLabel control={<Checkbox />} label="Daily sharing" />
+                <FormControlLabel control={<Checkbox />} label="Sports brands" />
+				<FormControlLabel control={<Checkbox />} label="Sporting events" />
             </FormControl>
         </Container>
     </div>
