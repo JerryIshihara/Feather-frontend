a76f50f58015c6d5fb025ef5471fef29002c0d5b
JerryIshihara <hirotaka.ishihara@mail.utoronto.ca>
2023-04-21 16:53:48 -0700

    modified pricing ui

--- src/routes/Dashboard/billing/payment.tsx
+++ src/routes/Dashboard/billing/payment.tsx
@@ -1,64 +1,53 @@
-import React from 'react';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Radio,
-} from '@mui/material';
+import React from "react";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Radio } from "@mui/material";
+import creditcard from "../../../assets/creditcard.png";
+import paypal from "../../../assets/paypal.png";
+import apple from "../../../assets/apple.png";
 
 const Payment = () => {
-  const [selectedOption, setSelectedOption] = React.useState('creditCard');
+	const [selectedOption, setSelectedOption] = React.useState("creditCard");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(event.target.value);
-  };
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSelectedOption(event.target.value);
+	};
 
-  const paymentOptions = [
-    {
-      id: 'creditCard',
-      label: 'Credit Card',
-      image: "../../../assets/creditcard.png",
-    },
-    {
-      id: 'applePay',
-      label: 'Apple Pay',
-      image: "../../../assets/apple.png",
-    },
-    {
-      id: 'paypal',
-      label: 'Paypal',
-      image: "../../../assets/paypal.png",
-    },
-  ];
+	const paymentOptions = [
+		{
+			id: "creditCard",
+			label: "Credit Card",
+			image: creditcard,
+		},
+		{
+			id: "applePay",
+			label: "Apple Pay",
+			image: apple,
+		},
+		{
+			id: "paypal",
+			label: "Paypal",
+			image: paypal,
+		},
+	];
 
-  return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableBody>
-          {paymentOptions.map((option) => (
-            <TableRow key={option.id}>
-              <TableCell>
-                <Radio
-                  checked={selectedOption === option.id}
-                  onChange={handleChange}
-                  value={option.id}
-                  name="payment-radio-button"
-                />
-                {option.label}
-              </TableCell>
-              <TableCell>
-                <img src={option.image} alt={option.label} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+	return (
+		<TableContainer component={Paper}>
+			<Table>
+				<TableBody>
+					{paymentOptions.map(option => (
+						<TableRow key={option.id}>
+							<TableCell>
+								<Radio checked={selectedOption === option.id} onChange={handleChange} value={option.id} name="payment-radio-button" />
+								{option.label}
+							</TableCell>
+							<TableCell>
+								<img src={option.image} alt={option.label} style={{ height: 40, objectFit: "contain" }} />
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 };
 
 export default Payment;
--- src/routes/Pricing/index.tsx
+++ src/routes/Pricing/index.tsx
@@ -1,130 +1,124 @@
-// import React from "react";
-// import { useTheme, Container } from "@mui/material";
-
-// const Pricing = () => {
-// 	const theme = useTheme();
-// 	return (
-// 		<Container maxWidth="lg">
-// 			<div style={{ color: theme.palette.text.primary }}>Pricing</div>
-// 		</Container>
-// 	);
-// };
-
 // export default Pricing;
 import React from "react";
-import { useTheme, Container, Grid, Typography, Button } from "@mui/material";
+import { useTheme, Container, Grid, Typography, Button, Stack, Divider, Box, Paper } from "@mui/material";
 import { styled } from "@mui/system";
+import { pink } from "@mui/material/colors";
+import { CheckCircleOutline } from "@mui/icons-material";
 
 const PricingContainer = styled(Container)(({ theme }) => ({
-  padding: theme.spacing(8, 0),
-  backgroundColor: theme.palette.background.paper,
+	padding: theme.spacing(8, 0),
+	backgroundColor: theme.palette.background.paper,
 }));
 
 const Pricing = () => {
-  const theme = useTheme();
-  return (
-    <PricingContainer maxWidth="lg">
-      <Typography variant="h3" align="center" gutterBottom>
-        Pricing Plans
-      </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={4}>
-          <PricingCard
-            title="Basic"
-            price="$9.99/month"
-            features={[
-              "Access to training videos",
-              "Video editing tools",
-              "Basic analysis",
-            ]}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <PricingCard
-            title="Pro"
-            price="$19.99/month"
-            features={[
-              "Access to training videos",
-              "Video editing tools",
-              "Advanced analysis",
-            ]}
-            popular
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <PricingCard
-            title="Premium"
-            price="$29.99/month"
-            features={[
-              "Access to training videos",
-              "Video editing tools",
-              "Expert analysis",
-            ]}
-          />
-        </Grid>
-      </Grid>
-    </PricingContainer>
-  );
+	const theme = useTheme();
+	return (
+		<PricingContainer maxWidth="lg">
+			<Stack direction="column" alignItems="center" spacing={2} sx={{ mb: 10 }}>
+				<Typography variant="h1" align="center" sx={{ fontWeight: "bold" }}>
+					Pricing Plans
+				</Typography>
+				<Typography variant="subtitle1" align="center" sx={{ color: theme.palette.text.secondary, maxWidth: "700px" }}>
+					Discover the perfect pricing plan tailored to your needs, offering value for money and exceptional quality for individuals, small
+					businesses, or large enterprises.
+				</Typography>
+			</Stack>
+			<Grid container spacing={4}>
+				<Grid item xs={12} md={4}>
+					<PricingCard
+						title="Basic"
+						price="Free"
+						features={["Video upload for replay", "Limited sports performance analysis", "Limited AI editing usage"]}
+					/>
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<PricingCard
+						title="Pro"
+						price="$19.99"
+						features={["Video upload for replay", "Sports performance analysis", "Unlimited AI editing usage"]}
+						popular
+					/>
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<PricingCard
+						title="Premium"
+						price="$29.99"
+						features={["Video upload for replay", "Sports advanced analysis", "Unlimited AI editing usage", "Team colloration"]}
+					/>
+				</Grid>
+			</Grid>
+		</PricingContainer>
+	);
 };
 
 interface PricingCardProps {
-  title: string;
-  price: string;
-  features: string[];
-  popular?: boolean;
+	title: string;
+	price: string;
+	features: string[];
+	popular?: boolean;
 }
 
 const PricingCard = ({ title, price, features, popular }: PricingCardProps) => {
-  const theme = useTheme();
-  return (
-    <div
-      style={{
-        backgroundColor: theme.palette.mode === "dark" ? "#333" : "#fff",
-        borderRadius: theme.shape.borderRadius,
-        boxShadow: theme.shadows[2],
-        padding: theme.spacing(4),
-        textAlign: "center",
-      }}
-    >
-      {popular && (
-        <Typography
-          variant="overline"
-          component="div"
-          style={{
-            backgroundColor: theme.palette.primary.main,
-            color: theme.palette.primary.contrastText,
-            borderRadius: theme.shape.borderRadius,
-            padding: theme.spacing(0.5, 2),
-            marginBottom: theme.spacing(2),
-          }}
-        >
-          Popular
-        </Typography>
-      )}
-      <Typography variant="h6" component="div" gutterBottom>
-        {title}
-      </Typography>
-      <Typography variant="h4" component="div" gutterBottom>
-        {price}
-      </Typography>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        {features.map((feature, index) => (
-          <li key={index} style={{ marginBottom: theme.spacing(1) }}>
-            {feature}
-          </li>
-        ))}
-      </ul>
-      <Button
-        variant="contained"
-        size="large"
-        color="primary"
-        fullWidth
-        style={{ marginTop: theme.spacing(2) }}
-      >
-        Sign Up
-      </Button>
-    </div>
-  );
+	const theme = useTheme();
+	const [elevation, setElevation] = React.useState(4);
+	return (
+		<Box component={"div"}>
+			<Paper key={elevation} elevation={elevation} sx={{ px: 5, py: 7 }}>
+				<Stack direction="column" spacing={2}>
+					<Stack direction="row" sx={{ alignItems: "center", justifyContent: "space-between" }} spacing={2}>
+						<Typography
+							variant="h6"
+							component="div"
+							sx={{
+								fontWeight: "bold",
+							}}
+						>
+							{title}
+						</Typography>
+						{popular && (
+							<div
+								style={{
+									backgroundColor: pink[400],
+									padding: "2px 10px",
+								}}
+							>
+								<Typography variant="subtitle2" sx={{ fontWeight: "600" }}>
+									Popular
+								</Typography>
+							</div>
+						)}
+					</Stack>
+
+					<Stack direction="row" sx={{ alignItems: "center" }} spacing={1}>
+						<Typography variant="h3" component="div">
+							{price}
+						</Typography>
+						<Typography variant="body2" component="div" sx={{ color: theme.palette.text.secondary }}>
+							/ month
+						</Typography>
+					</Stack>
+					<Divider sx={{ py: 2 }} />
+
+					<Typography variant="body2" component="div" sx={{ fontWeight: "bold" }}>
+						Features
+					</Typography>
+					<Stack direction="column" spacing={1}>
+						{features.map((feature, index) => (
+							<Stack direction="row" spacing={1} key={index}>
+								<CheckCircleOutline sx={{ color: theme.palette.text.primary, fontSize: 18 }} />
+								<Typography variant="body2" sx={{ fontWeight: "500" }}>
+									{feature}
+								</Typography>
+							</Stack>
+						))}
+					</Stack>
+				</Stack>
+				<Button variant="contained" size="large" color="primary" fullWidth sx={{ mt: 5, borderRadius: 0 }}>
+					Upgrade
+				</Button>
+			</Paper>
+		</Box>
+	);
 };
 
 export default Pricing;
