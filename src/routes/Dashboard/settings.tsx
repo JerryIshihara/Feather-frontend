a78904bbc82c768247a24bb45f5c0d0563394eae
xuegongao <gongao_xue@berkeley.edu>
2023-04-25 15:01:26 -0700

    debug

--- src/routes/Dashboard/settings.tsx
+++ src/routes/Dashboard/settings.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useTheme, TextField, Container, FormControlLabel, Checkbox, Typography, Divider, RadioGroup, Radio, FormControl, FormLabel, Button} from "@mui/material";
 import SaveIcon from '@mui/icons-material/Save';
-import { useTheme, TextField, Container, FormControlLabel, Checkbox, Typography, Divider, RadioGroup, Radio, FormControl, FormLabel, Button} from "@mui/material";
-import SaveIcon from '@mui/icons-material/Save';
 
 const Setting = () => {
 const theme = useTheme();
@@ -10,38 +8,6 @@ const theme = useTheme();
 return (
     <div style={{ backgroundColor: theme.palette.background.default, padding: theme.spacing(4) }}>
         <Container maxWidth="lg">
-
-			{/* -----------------------------------------Password----------------------------------------- */}
-			<Typography variant="h4" gutterBottom>
-				Change Password
-				<div style={{ float: 'right' }}>
-					<Button variant="contained" sx={{ color: theme.palette.text.primary }} startIcon={<SaveIcon />} component="span">
-						Save
-					</Button>
-				</div>
-				<div>
-					<TextField label="Current Password" required type="password" margin="normal" variant="outlined" fullWidth />
-					<TextField label="New Password" type="password" margin="normal" variant="outlined" fullWidth inputProps={{ minLength: 8 }} />
-					<TextField label="Confirm New Password" type="password" margin="normal" variant="outlined" fullWidth inputProps={{ minLength: 8 }} />
-				</div>
-			</Typography>
-
-			{/* -----------------------------------------Contact Info----------------------------------------- */}
-			<Divider style={{ margin: theme.spacing(2, 0) }} />
-			<Typography variant="h4" gutterBottom>
-				Contact Information
-				<div style={{ float: 'right' }}>
-					<Button variant="contained" sx={{ color: theme.palette.text.primary }} startIcon={<SaveIcon />} component="span">
-						Save
-					</Button>
-				</div>
-				<div>
-					<TextField label="Email" required type="email" margin="normal" variant="outlined" fullWidth/>
-					<TextField label="Phone Number" type="tel" margin="normal" variant="outlined" fullWidth/>
-				</div>
-			</Typography>
-
-			{/* -----------------------------------------Notification----------------------------------------- */}
 
 			{/* -----------------------------------------Password----------------------------------------- */}
 			<Typography variant="h4" gutterBottom>
@@ -83,19 +49,9 @@ return (
 					</Button>
 				</div>
 			</Typography>
-            <Typography variant="h4" gutterBottom>
-				Notifications
-				<div style={{ float: 'right' }}>
-					<Button variant="contained" sx={{ color: theme.palette.text.primary }} startIcon={<SaveIcon />} component="span">
-						Save
-					</Button>
-				</div>
-			</Typography>
             <FormControl component="fieldset">
 				<Typography variant="h5">Receive Notifications:</Typography>
                 <RadioGroup aria-label="notifications" name="notifications" defaultValue="email">
-					<FormControlLabel value="email" control={<Checkbox />} label="Email" />
-					<FormControlLabel value="text" control={<Checkbox />} label="Text" />
 					<FormControlLabel value="email" control={<Checkbox />} label="Email" />
 					<FormControlLabel value="text" control={<Checkbox />} label="Text" />
                 </RadioGroup>
@@ -108,14 +64,7 @@ return (
                 <FormControlLabel control={<Checkbox />} label="New comments" />
 				<FormControlLabel control={<Checkbox />} label="Advertisements" />
 				<FormControlLabel control={<Checkbox />} label="Sport lover events and meetups" />
-                <FormControlLabel control={<Checkbox />} label="Product updates" />
-                <FormControlLabel control={<Checkbox />} label="New videos" />
-                <FormControlLabel control={<Checkbox />} label="New comments" />
-				<FormControlLabel control={<Checkbox />} label="Advertisements" />
-				<FormControlLabel control={<Checkbox />} label="Sport lover events and meetups" />
             </FormControl>
-
-			{/* -----------------------------------------Preference----------------------------------------- */}
 
 			{/* -----------------------------------------Preference----------------------------------------- */}
             <Divider style={{ margin: theme.spacing(2, 0) }} />
