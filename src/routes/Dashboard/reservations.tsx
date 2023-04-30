8ea3af00a878e9438b139d89e9938718aa70c378
xuegongao <gongao_xue@berkeley.edu>
2023-04-30 09:25:41 -0700

    detail presentation page

--- src/routes/Dashboard/reservations.tsx
+++ src/routes/Dashboard/reservations.tsx
@@ -1,8 +1,31 @@
-import React, { useState, useEffect } from "react";
-import { useTheme, Button, Table, TableBody, TableCell, TableContainer, TableRow, Paper, Typography, Container, DialogContentText, DialogTitle, Box, Dialog, Stack, MenuItem, TextField, DialogContent, DialogActions} from "@mui/material";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  CardActions,
+  Card,
+  CardContent,
+  CardMedia,
+  useTheme,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  Typography,
+  Container,
+  DialogContentText,
+  DialogTitle,
+  Box,
+  Dialog,
+  Stack,
+  MenuItem,
+  TextField,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 import WarningAmberIcon from "@mui/icons-material/WarningAmber";
-import { useAuth } from "../../contexts/auth";
 
 import court1 from "./assets/court1.jpg";
 import court2 from "./assets/court2.jpg";
@@ -10,56 +33,67 @@ import court3 from "./assets/court3.jpg";
 import court4 from "./assets/court4.jpg";
 import court5 from "./assets/court5.jpg";
 
+import { AuthContext } from "../../contexts/auth";
+
 type Reservation = {
-	id: number;
-	field_name: string;
-	court_number: string;
-	date: string;
-	start_time: string;
-	end_time: string;
-	address:  string;
-	price: string;
-	description: string;
-	image?: string;
+  id: number;
+  field_name: string;
+  court_number: string;
+  date: string;
+  start_time: string;
+  end_time: string;
+  address: string;
+  price: string;
+  description: string;
+  image?: string;
 };
 
 const Reservations = () => {
-	const auth = useAuth() as any;
-	const theme = useTheme();
-	const user_id = auth.user.attributes.sub;
-	const [reservations, setReservations] = useState<Reservation[]>([]);
-	const [selectedReservation, setSelectedReservation] = useState(null);
-	const [open, setOpen] = useState(false);
+  const auth = useContext(AuthContext) as any;
+  const theme = useTheme();
+  const user_id = auth.user.attributes.sub;
+  const [reservations, setReservations] = useState<Reservation[]>([]);
+  const [selectedReservation, setSelectedReservation] = useState<Reservation>();
+  const [open, setOpen] = useState(false);
 
-	useEffect(() => {
-		const fetchReservations = async () => {
-			const response = await fetch("/api/getReserve?user_id=" + user_id);
-			// const response = await fetch("http://localhost:8000/api/booking/getReserve?user_id=" + user_id);
-			const data = await response.json();
-			const reservationWithImages = data.courts.map((court: Reservation) => {
-				let image;
-				switch (court.field_name) {
-				  case "Elite Badminton":
-					image = court1;
-					break;
-				  case "Eastbay Badminton":
-					image = court2;
-					break;
-				  case "Bintang Badminton":
-					image = court3;
-					break;
-				  case "Synergy Badminton":
-					image = court4;
-					break;
-				  default:
-					image = court5;
-				}
-				return { ...court, image };
-			  });
-			  setReservations(reservationWithImages);
-		};
-		fetchReservations();
-	}, []);
+  const handleDetailsClick = (reservation: Reservation) => {
+	setSelectedReservation(reservation);
+	setOpen(true);
+  };
+
+  useEffect(() => {
+    const fetchReservations = async () => {
+    //   const response = await fetch("http://localhost:8000/api/booking/getReserve?user_id=" + user_id);
+	  const response = await fetch("/api/getReserve?user_id=" + user_id);
+      const data = await response.json();
+      const reservationWithImages = data.courts.map((court: Reservation) => {
+        let image;
+        switch (court.field_name) {
+          case "Elite Badminton":
+            image = court1;
+            break;
+          case "Eastbay Badminton":
+            image = court2;
+            break;
+          case "Bintang Badminton":
+            image = court3;
+            break;
+          case "Synergy Badminton":
+            image = court4;
+            break;
+          default:
+            image = court5;
+        }
+        return { ...court, image };
+      });
+      setReservations(reservationWithImages);
+    };
+    fetchReservations();
+  }, []);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
 	return (
 		<Container maxWidth="lg" sx={{ py: 4 }}>
@@ -69,23 +103,58 @@ const Reservations = () => {
 			<TableContainer component={Paper}>
 				<Table>
 					<TableBody>
-						{reservations.map((reservation, index) => (
-							<TableRow key={index} sx={{ py: 4, px: 2 }}>
-								<TableCell>
-									<Typography variant="body1">{reservation.field_name}</Typography>
-								</TableCell>
-								<TableCell>{reservation.date}&nbsp;{reservation.start_time}&nbsp;-&nbsp;{reservation.end_time}</TableCell>
-								<TableCell>{reservation.address}</TableCell>
-								<TableCell align="right">
-									<Button variant="contained" sx={{ mr: 2 }} startIcon={<InfoIcon />}>
-										Details
-									</Button>
-									<Button variant="contained" color="error" startIcon={<WarningAmberIcon />}>
-										Cancel
-									</Button>
-								</TableCell>
-							</TableRow>
-						))}
+					{reservations.map((reservation: Reservation) => (
+						<TableRow key={reservation.id} sx={{ py: 4, px: 2 }}>
+							<TableCell>
+								<Typography variant="body1">{reservation.field_name}</Typography>
+							</TableCell>
+							<TableCell>{reservation.date}&nbsp;{reservation.start_time}&nbsp;-&nbsp;{reservation.end_time}</TableCell>
+							<TableCell>{reservation.address}</TableCell>
+							<TableCell align="right">
+								<Button variant="contained" sx={{ mr: 2 }} startIcon={<InfoIcon />} onClick={() => handleDetailsClick(reservation)}>
+									Details
+								</Button>
+								<Button variant="contained" color="error" startIcon={<WarningAmberIcon />}>
+									Cancel
+								</Button>
+							</TableCell>
+						</TableRow>
+					))}
+					{selectedReservation && (
+						<Dialog open={open} onClose={handleClose}>
+							<DialogTitle sx={{backgroundColor: '##1e1e1e'}}>{selectedReservation.field_name}</DialogTitle>
+							<DialogContent sx={{backgroundColor: '##1e1e1e'}}>
+								<Stack direction="column" spacing={2}>
+									<img src={selectedReservation.image || "court.jpg"} alt={selectedReservation.field_name}/>
+									<Stack direction="column" spacing={1}>
+										<Stack direction="row" justifyContent="space-between">
+											<Typography variant="body1"><b>Court Number:</b></Typography>
+											<Typography variant="body1">{selectedReservation.court_number}</Typography>
+										</Stack>
+										<Stack direction="row" justifyContent="space-between">
+											<Typography variant="body1"><b>Available Time:</b></Typography>
+											<Typography variant="body1">{selectedReservation.date} &nbsp; {selectedReservation.start_time} - {selectedReservation.end_time}</Typography>
+										</Stack>
+										<Stack direction="row" justifyContent="space-between">
+											<Typography variant="body1"><b>Address:</b></Typography>
+											<Typography variant="body1">{selectedReservation.address}</Typography>
+										</Stack>
+										<Stack direction="row" justifyContent="space-between">
+											<Typography variant="body1"><b>Price:</b></Typography>
+											<Typography variant="body1">{selectedReservation.price} &nbsp; $</Typography>
+										</Stack>
+										<Stack direction="row" justifyContent="space-between">
+											<Typography variant="body1"><b>Description:</b></Typography>
+											<Typography variant="body1">{selectedReservation.description}</Typography>
+										</Stack>
+									</Stack>
+								</Stack>
+							</DialogContent>
+							<DialogActions>
+								<Button onClick={handleClose}>Close</Button>
+							</DialogActions>
+						</Dialog>
+						)}
 					</TableBody>
 				</Table>
 			</TableContainer>
